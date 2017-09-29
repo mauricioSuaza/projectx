5551981c95935b776a48f27cceb0fe23ffd68b6d
kevanz10 <krojas1171@gmail.com>
2017-09-29 12:57:55 -0500

    cleaned worker and controller method

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,34 +1,27 @@
 class TransactionsController < ApplicationController
     
-    def add_invoice
-        @transaction = Transaction.find(params[:transaction_id])
-        @transaction.invoice = params[:invoice]
-        @transaction.save!
-        redirect_to '/my_dashboard', notice: "image succesfully saved"
-    end
+  def add_invoice
+    @transaction = Transaction.find(params[:transaction_id])
+    @transaction.invoice = params[:invoice]
+    @transaction.save!
+    redirect_to '/my_dashboard', notice: "image succesfully saved"
+  end
 
-    def confirm_transaction
-        @transaction = Transaction.find(params[:transaction_id])
-        
-        if @transaction.receiver_id == current_user.id
-            #@transaction.update(status: true)
-            #@sender = User.find(@transaction.sender_id)
-            #@sender.update(saldo:  (@sender.saldo + (@transaction.value + (@transaction.value*0.3))))
-            #redirect_to '/my_dashboard', notice: "transaction sucesfully confirmed"
-            UserWorker.perform_in(1.minutes, @transaction.id)
-            flash[:notice] = "Transaction sucesfully confirmed!. Your total balance will be updated 
-                in 15 days, thank you for trusting in us."
-            redirect_to '/my_dashboard'
-            
-        else
-            redirect_to '/my_dashboard', notice: "you don't have permission to acces this transaction"
-        end
-       
+  def confirm_transaction
+    @transaction = Transaction.find(params[:transaction_id])
+    
+    if @transaction.receiver_id == current_user.id
+      UserWorker.perform_in(1.minutes, @transaction.id)
+      flash[:notice] = "Transaction sucesfully confirmed!. Your total balance will be updated 
+          in 15 days, thank you for trusting in us."
+      redirect_to '/my_dashboard'  
+    else
+      redirect_to '/my_dashboard', notice: "you don't have permission to acces this transaction"
     end
+  end
 
 private
-    def invoice_params
-      params.permit(:invoice)
-    end
-
+  def invoice_params
+    params.permit(:invoice)
+  end
 end
--- app/workers/user_worker.rb
+++ app/workers/user_worker.rb
@@ -3,11 +3,9 @@ class UserWorker
 	sidekiq_options retry: true
 
 	def perform(transaction_id)
-		#user = User.find(user_id)
-    #user.update_attributes(name: 'testing 2')
     @transaction = Transaction.find(transaction_id)
     @transaction.update(status: true)
     @sender = User.find(@transaction.sender_id)
-    @sender.update(saldo:  (@sender.saldo + (@transaction.value + (@transaction.value*0.3))))    
+    @sender.update(saldo: (@sender.saldo + (@transaction.value + (@transaction.value*0.3))))    
 	end
 end
