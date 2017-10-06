e10e1d00a0e35d5b9c104ee8fc75dced6f31abf1
kevanz10 <krojas1171@gmail.com>
2017-10-06 16:46:40 -0500

    added new initializers

--- config/puma.rb
+++ config/puma.rb
@@ -4,16 +4,20 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)  
+threads_count = Integer(ENV['MAX_THREADS'] || 1)  
+
 threads threads_count, threads_count
 
+preload_app!
+
+rackup      DefaultRackup  
+port        ENV['PORT']     || 3000  
+environment ENV['RACK_ENV'] || 'development'
+
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -30,8 +34,20 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
+
 # preload_app!
 
+connections to disconnect, so it should be fine.
+before_fork do  
+  puts "Puma master process about to fork. Closing existing Active record connections."
+  ActiveRecord::Base.connection.disconnect!
+end
+
+on_worker_boot do  
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end  
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
 # process is booted this block will be run, if you are using `preload_app!`
@@ -44,4 +60,3 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
