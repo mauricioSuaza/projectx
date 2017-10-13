99783be8c18fd3ec62923997b33528b99a902fc5
MAURICIO SUAZA <mauriciosuazauribe@gmail.com>
2017-10-13 17:59:54 -0500

    added second seed

---
+++ db/seeds.rb
@@ -0,0 +1,185 @@
+owner1 = User.new(name: 'Jhonatan Ruiz', email: 'jhon1839@outlook.com',phone: '3015732977', btc: '3FJMB4oAZkU1x61AXokzUmSgQxL2xaeeRb', password: 'asdasd123456')
+
+owner1.add_role :owner
+
+owner1.skip_confirmation!
+
+owner1.save!
+
+owner2 = User.new(name: 'Selina Marquez', email: 'sagitario6457@outlook.com',phone: '3015732858', btc: '3EqRVikMseAr7RFzWAZWWvWiTSbiPPHoXd', password: 'asdasd123456')
+
+owner2.add_role :owner
+
+owner2.skip_confirmation!
+
+owner2.save!
+
+owner3 = User.new(name: 'Paola López', email: 'politabitcoin@gmail.com', phone: '3015645842', btc: '3FcuSpZVyo1pNsLBQW9aMbdJfFnYdey43a', password: 'asdasd123456')
+
+owner3.add_role :owner
+
+owner3.skip_confirmation!
+
+owner3.save!
+
+owner4 = User.new(name: 'Pipe Pérez', email: ' juanfelipeperez21@gmail.com',phone: '3015734152', btc: '3KFSKrejgyvbLmu7yCUGBjEUDGBSWxRNWm', password: 'asdasd123456')
+
+owner4.add_role :owner
+
+owner4.skip_confirmation!
+
+owner4.save!
+
+owner5 = User.new(name: 'Camila Restrepo', email: 'camilarestrepocano12@gmail.com',phone: '3015732259', btc: '36sy16NpjqKSRv3ZPHqVnVKi22DDkGWw7d', password: 'asdasd123456')
+
+owner5.add_role :owner
+
+owner5.skip_confirmation!
+
+owner5.save!
+
+ 
+
+owner6 = User.new(name: 'María Monsalve', email: 'mariamonsalve12@outlook.com',phone: '3015707472', btc: '3FFGhNRhc5ZBkqr1HPPxmDespJjpN2mVz3', password: 'asdasd123456')
+
+owner6.add_role :owner
+
+owner6.skip_confirmation!
+
+owner6.save!
+
+ 
+
+owner7 = User.new(name: 'Johann Ramirez', email: 'johanaramirez01@outlook.com',phone: '3015732916', btc: '3GgLW5az43oYeQ2x9NV2vxwnPEy8GTTr3Y', password: 'asdasd123456')
+
+owner7.add_role :owner
+
+owner7.skip_confirmation!
+
+owner7.save!
+
+  
+
+owner8 = User.new(name: 'Gina Mesa', email: 'ginamesa@outlook.com',phone: '3007834802', btc: '3HGDXhb5wFdquDuixN1fFKepXbCrUxAyXK', password: 'asdasd123456')
+
+owner8.add_role :owner
+
+owner8.skip_confirmation!
+
+owner8.save!
+
+owner9 = User.new(name: 'Viviana Torres', email: 'vivitorresh24@hotmail.com',phone: '3007834554', btc: '3DgZ8ktZfcadqx8P5YAHP7JEkdevXCSjwo', password: 'asdasd123456')
+
+owner9.add_role :owner
+
+owner9.skip_confirmation!
+
+owner9.save!
+
+   
+
+owner10 = User.new(name: 'Pablito Correa', email: 'correapablo@yahoo.com',phone: '3015761744', btc: '35nKKZfmvr8zEvJW3uAYTE6UB7dye6tfpg', password: 'asdasd123456')
+
+owner10.add_role :owner
+
+owner10.skip_confirmation!
+
+owner10.save!
+
+owner11 = User.new(name: 'Belisario Velásquez', email: 'belisario.velasquez@yahoo.com',phone: '3007833696', btc: '3KNxPKsLutszXf1NuTFJmzZ2mcndB79mUX', password: 'asdasd123456')
+
+owner11.add_role :owner
+
+owner11.skip_confirmation!
+
+owner11.save!
+
+owner12 = User.new(name: 'Daniel Moncada', email: 'danielmoncada03@outlook.com',phone: '3007826593', btc: '3BHRsWmH185Kfo43JFuX1owvS6ctKei4pA', password: 'asdasd123456')
+
+owner12.add_role :owner
+
+owner12.skip_confirmation!
+
+owner12.save!
+
+owner13 = User.new(name: 'Camilo Cortes Mesa', email: 'camiloc40@yahoo.com',phone: '3007833608', btc: '3NHoFAyzLME2VpwQVJjGpRKLUvWoGfJ1xV', password: 'asdasd123456')
+
+owner13.add_role :owner
+
+owner13.skip_confirmation!
+
+owner13.save!
+
+owner14 = User.new(name: 'Gloria Florez', email: 'gloriaflores27@outlook.com',phone: '3007825791', btc: '37zh6fFtWHgbSzrozNGCesTHKwQrjpgVps', password: 'asdasd123456')
+
+owner14.add_role :owner
+
+owner14.skip_confirmation!
+
+owner14.save!
+
+owner15 = User.new(name: 'Manuel Muriel', email: 'manuelamuriel03@outlook.com',phone: '3007825679', btc: '362abnuTWkkpmZoy6XsKWQYWAiuJ5DnQfv', password: 'asdasd123456')
+
+owner15.add_role :owner
+
+owner15.skip_confirmation!
+
+owner15.save!
+
+owner16 = User.new(name: 'Daniel Perez', email: 'danielperez@outlook.com',phone: '3007833568', btc: '33pKeJPdEjPWBhAKcSjRnN4fQFqfsjHW1A', password: 'asdasd123456')
+
+owner16.add_role :owner
+
+owner16.skip_confirmation!
+
+owner16.save!
+
+owner17 = User.new(name: 'Julie Florez', email: 'Julie27@outlook.com',phone: '3015763093', btc: '32row31L6w6CKVPc6AsDdu3n179CTTmZTB', password: 'asdasd123456')
+
+owner17.add_role :owner
+
+owner17.skip_confirmation!
+
+owner17.save!
+
+owner18 = User.new(name: 'Manuel Trujillo', email: 'trujillomanuel90@outlook.com',phone: '3007826157', btc: '352VvRa1pVB6USWndLLTCpDuCHeq9M6bdD', password: 'asdasd123456')
+
+owner18.add_role :owner
+
+owner18.skip_confirmation!
+
+owner18.save!
+
+owner19 = User.new(name: 'Julio Valenzuela', email: 'juliovalenzuela848@yahoo.com',phone: '3015645673', btc: '3DQYDuivNgJ5Jc1fheEe5nni4mmHUvGawR', password: 'asdasd123456')
+
+owner19.add_role :owner
+
+owner19.skip_confirmation!
+
+owner19.save!
+
+owner20 = User.new(name: 'Karen Galeano Aristizabal', email: 'gkaren399@yahoo.com', phone: '3015645582', btc: '35gLDmZEnkpYVuhtro1akf9o7jdFFa3Ljz', password: 'asdasd123456')
+
+owner20.add_role :owner
+
+owner20.skip_confirmation!
+
+owner20.save!
+
+owner21 = User.new(name: 'Arley Carrasquilla', email: 'arleycarrasquilla@yahoo.com',phone: '3015645541', btc: '3AsGreQwJSYJk8MvQCbwuK2p3Q58aQKzb7', password: 'asdasd123456')
+
+owner21.add_role :owner
+
+owner21.skip_confirmation!
+
+owner21.save!
+
+    
+
+owner22 = User.new(name: 'Sandro Campo', email: 'san_ocampo_m11@hotmail.com',phone: '3015671215', btc: '3Abb2fYydWVJ5UHRRvKRFs7dbM92jh1SST', password: 'asdasd123456')
+
+owner22.add_role :owner
+
+owner22.skip_confirmation!
+
+owner22.save!
