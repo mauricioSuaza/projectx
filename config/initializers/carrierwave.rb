f0f25b77939df01581ec50aa2441a3e3d67de901
MAURICIO SUAZA <mauriciosuazauribe@gmail.com>
2017-12-22 12:40:02 -0500

    switched amazon bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,30 +5,26 @@ require 'carrierwave/storage/fog'
   CarrierWave.configure do |config|
 
 
-    if Rails.env.development? || Rails.env.test?
-      CarrierWave.configure do |config|
-        config.storage = :file
-      end
-    end
+  
 
-    if Rails.env.production?
+   
       CarrierWave.configure do |config|
         config.storage = :fog
         config.fog_credentials = {
           :provider               => 'AWS',                            # required
-          :aws_access_key_id      => ENV['AWS_ID'],                     # required
-          :aws_secret_access_key  => ENV['AWS_KEY'],
+          :aws_access_key_id      => ENV['AWS_ID_TWO'],                     # required
+          :aws_secret_access_key  => ENV['AWS_KEY_TWO'],
           :region                 => 'us-west-2'
         }
        
-        config.fog_directory  = 'donatingoals'                    # required
+        config.fog_directory  = 'goalsdon'                    # required
         config.fog_public     = true                                   # optional, defaults to true
         config.root = Rails.root.join('tmp')
         config.cache_dir = 'files'
         config.permissions = 0777
         config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} 
       end
-    end
+   
 
  # optional, defaults to {}
   end
